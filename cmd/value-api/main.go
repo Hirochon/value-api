813e7c19f2c15eefab3bb4761765e345779ac3f9
Hirochon <heacet43@gmail.com>
2022-07-25 02:05:04 +0900

    add: logger処理を追加

--- cmd/value-api/main.go
+++ cmd/value-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/Hirochon/value-api/pkg/logger"
 )
 
 // main は、context.Context を初期化して、アプリケーションを起動する
@@ -12,5 +13,16 @@ func main() {
 
 // run は、アプリケーションを起動する(ログ起動、SQLサーバー起動、APIサーバー起動)
 func run(ctx context.Context) {
-	fmt.Println("Hello, world!")
+	l, err := logger.New()
+	if err != nil {
+		panic(fmt.Sprintf("failed to create logger: %s", err))
+	}
+	valueApiLogger := l.WithName("value-api")
+	valueApiLogger.Info("starting up value-api")
+
+	//mysqlClient, err := initialization.NewMySQLClient()
+	//if err != nil {
+	//	panic(fmt.Sprintf("failed to create mysql client: %s", err))
+	//}
+	//defer mysqlClient.Close()
 }
