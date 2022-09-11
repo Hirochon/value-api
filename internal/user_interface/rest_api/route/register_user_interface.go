3f73cb66d37046ec7b8c23d5cd376ced92a71352
Hirochon <heacet43@gmail.com>
2022-09-12 03:28:36 +0900

    add: register user interface

---
+++ internal/user_interface/rest_api/route/register_user_interface.go
@@ -0,0 +1,31 @@
+package route
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type registerUser struct {
+	Name     string `json:"name"`
+	Gender   int    `json:"gender"`
+	BirthDay string `json:"birthDay"`
+}
+
+// RegisterUser は、ユーザーを登録する
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("Error IO: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	parsedData, err := unmarshalToStruct[registerUser](data)
+	if err != nil {
+		fmt.Printf("Error parse data: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%+v\n", parsedData)
+	w.WriteHeader(http.StatusCreated)
+}
