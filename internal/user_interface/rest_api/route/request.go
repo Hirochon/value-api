9f1ee41115c6efc82c2994e518395487dad168fe
Hirochon <heacet43@gmail.com>
2022-09-12 03:28:17 +0900

    add: unmarshaler for all requests

---
+++ internal/user_interface/rest_api/route/request.go
@@ -0,0 +1,15 @@
+package route
+
+import "encoding/json"
+
+type requestBody interface {
+	registerUser
+}
+
+func unmarshalToStruct[T requestBody](body []byte) (T, error) {
+	var t T
+	if err := json.Unmarshal(body, &t); err != nil {
+		return t, err
+	}
+	return t, nil
+}
